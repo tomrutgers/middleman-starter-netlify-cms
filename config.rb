0d84018c5314073e558ee3a0c48ec8a7e8c85b19
Tom Rutgers <tom@steam.nl>
2018-08-15 01:16:23 +0200

    Add documentation for the use of bootstrap

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
+#Bootstrap is used to style bits of the demo. Remove it from the config, gemfile and stylesheets to stop using bootstrap
 require 'bootstrap-sass'
+
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
